796162ff6eddd54c0a640843ecbf018ee73e93df
damian <adamodamian6@gmail.com>
2024-03-07 18:41:32 -0300

    implementando swagger

--- banquetes/src/main/java/com/banquetes/banquetes/controller/UsuarioController.java
+++ banquetes/src/main/java/com/banquetes/banquetes/controller/UsuarioController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.*;
 
 @RestController
+@RequestMapping("/usuarios")
 @CrossOrigin(origins = "*")
 public class UsuarioController {
     @Autowired
