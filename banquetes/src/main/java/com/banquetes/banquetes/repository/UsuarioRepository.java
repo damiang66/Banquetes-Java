890afdeade9894ac17821f8b0a98c0baac2d7bbb
damian <adamodamian6@gmail.com>
2024-03-07 19:16:08 -0300

    seguridad y swagger

--- banquetes/src/main/java/com/banquetes/banquetes/repository/UsuarioRepository.java
+++
@@ -1,8 +0,0 @@
-package com.banquetes.banquetes.repository;
-
-import com.banquetes.banquetes.entity.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario,Long> {
-
-}
